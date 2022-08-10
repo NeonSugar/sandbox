e503d5589b7e74eb9cb3723d7bc3f6b0a4a59094
NeonSugar <zzxxim@gmail.com>
2022-08-10 16:03:59 +0300

    Added Math Util

---
+++ NeonSugar.Sandbox/NeonSugar.Sandbox/Utils/Math.cs
@@ -0,0 +1,11 @@
+﻿namespace NeonSugar.Sandbox.Utils;
+
+public static class Math
+{
+	public static T? Min<T>(params T[] numbers) where T : IComparable
+		=> numbers.Min();
+	public static T? Max<T>(params T[] numbers) where T : IComparable
+		=> numbers.Max();
+
+	
+}
